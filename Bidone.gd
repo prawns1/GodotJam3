5612007c649ad155629c7f20d14e0fc6020f0043
Gabry <gabryxmastergamer@gmail.com>
2020-11-01 21:49:11 +0100

    BIdoni movimento2

--- Bidone.gd
+++ Bidone.gd
@@ -13,7 +13,7 @@ var textures := {
 }
 
 export var active = false
-
+var posizione=0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -48,7 +48,18 @@ func _physics_process(_delta):
 	if active==true:
 		var velocity=Vector2()
 		if Input.is_key_pressed(KEY_RIGHT):
-			velocity.x=+100
+			velocity.x=+500
 		if Input.is_key_pressed(KEY_LEFT):
-				 velocity.x=-100
+				 velocity.x=-500
+		if Input.is_action_just_pressed("ui_up"):
+			if posizione==1:
+				position.y-=40
+				position.x+=40
+				posizione=0
+		if Input.is_action_just_pressed("ui_down"):
+			if posizione==0:
+				position.y+=40
+				position.x-=40
+				posizione=1
 		move_and_slide(velocity)
+		
